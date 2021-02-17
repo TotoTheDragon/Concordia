87a549f0e3c9a7faf2972b2e398632576865e52e
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2021-02-17 17:07:52 +0100

    Added an util for logging

---
+++ src/Logger.ts
@@ -0,0 +1,62 @@
+import EventEmitter from "events";
+
+export class SimpleLogger extends EventEmitter {
+
+    _debug: boolean;
+
+    constructor(debug: boolean = false) {
+        super();
+        this.debugEnabled = debug;
+        this.startListeners("LOG", "WARN", "ERROR");
+    }
+
+    disableDebug() {
+        this._debug = false;
+        this.removeAllListeners("DEBUG");
+    }
+
+    enableDebug() {
+        this._debug = true;
+        this.startListeners("DEBUG");
+    }
+
+    set debugEnabled(debug: boolean) {
+        debug ? this.enableDebug() : this.disableDebug();
+    }
+
+    get debugEnabled() {
+        return this._debug;
+    }
+
+    startListeners(...args: LogType[]) {
+        if (!args.length || args.includes("LOG"))
+            this.on("LOG", (type, ...arg) => console.log(ConsoleColors.FgBlue + `[${type}]`, ...(arg.map(a => a.toString())), ConsoleColors.Reset));
+        if (!args.length || args.includes("WARN"))
+            this.on("WARN", (type, ...arg) => console.warn(ConsoleColors.FgMagenta + `[${type}]`, ...(arg.map(a => a.toString())), ConsoleColors.Reset))
+        if (!args.length || args.includes("ERROR"))
+            this.on("ERROR", (type, ...arg) => console.error(ConsoleColors.FgRed + `[${type}]`, ...(arg.map(a => a.toString())), ConsoleColors.Reset))
+        if (args.includes("DEBUG"))
+            this.on("DEBUG", (type, ...arg) => console.debug(ConsoleColors.FgYellow + `[${type}]`, ...(arg.map(a => a.toString())), ConsoleColors.Reset))
+    }
+}
+
+export type LogType = "LOG" | "WARN" | "ERROR" | "DEBUG";
+
+export enum ConsoleColors {
+    Reset = "\x1b[0m",
+    Bright = "\x1b[1m",
+    Dim = "\x1b[2m",
+    Underscore = "\x1b[4m",
+    Blink = "\x1b[5m",
+    Reverse = "\x1b[7m",
+    Hidden = "\x1b[8m",
+
+    FgBlack = "\x1b[30m",
+    FgRed = "\x1b[31m",
+    FgGreen = "\x1b[32m",
+    FgYellow = "\x1b[38;2;210;190;60m",
+    FgBlue = "\x1b[34m",
+    FgMagenta = "\x1b[35m",
+    FgCyan = "\x1b[36m",
+    FgWhite = "\x1b[37m",
+}
