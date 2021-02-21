2d31a0ae83d4479df7d2b14d3802f3f732284cd5
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2021-02-21 13:21:41 +0100

    Added a way to get plugin

--- src/plugins/PluginManager.ts
+++ src/plugins/PluginManager.ts
@@ -12,6 +12,10 @@ export class PluginManager {
         this.registered = new Map();
     }
 
+    getPlugin<T extends ServerPlugin>(identifier: string): T {
+        return this.registered.get(identifier) as T;
+    }
+
     registerPlugin(plugin: ServerPlugin): void {
         if (this.registered.has(plugin.identifier)) throw new Error("Trying to register plugin that has already been registered: " + plugin.identifier);
         plugin.initialize(this.manager);
